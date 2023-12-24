8976c22ceac729c62ed8cb29ef93a229b4dbbab8
ai-max16 <renn8q8@gmail.com>
2023-12-24 19:21:20 +0900

    kadai_002を提出

---
+++ kadai_002/laravel-samuraimart/resources/views/web/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+  <div class="col-2">
+    @component('components.sidebar', ['categories' => $categories, 'major_categories' => $major_categories])
+    @endcomponent
+  </div>
+  <div class="col-9">
+    <h1>おすすめ商品</h1>
+    <div class="row">
+      @foreach ($recommend_products as $recommend_product)
+      <div class="col-4">
+          <a href="{{ route('products.show', $recommend_product) }}">
+            @if ($recommend_product->image !== "")
+            <img src="{{ asset($recommend_product->image) }}" class="img-thumbnail">
+            <div class="d-flex">  
+              @if ($recommend_product->reviews()->exists())
+              <p class="star-rating" data-rate="{{ round(2 * $recommend_product->reviews->avg('score')) / 2 }}"></p>
+              <p class="review-count">{{ $recommend_product->reviews->count() }}</p>
+              @endif
+            </div>
+            @else
+            <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+            @endif
+          </a>
+          <div class="row">
+            <div class="col-12">
+              <p class="samuraimart-product-label mt-2">
+                {{ $recommend_product->name }}<br>
+                <label>¥{{ $recommend_product->price }}</label>
+              </p>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+    
+    <h1>新着商品</h1>
+    <div class="row">
+      @foreach ($recently_products as $recently_product)
+        <div class="col-3">
+          <a href="{{ route('products.show', $recently_product) }}">
+            @if ($recently_product->image !== "")
+            <img src="{{ asset($recently_product->image) }}" class="img-thumbnail">
+            <div class="d-flex">  
+              @if ($recently_product->reviews()->exists())
+              <p class="star-rating" data-rate="{{ round(2 * $recently_product->reviews->avg('score')) / 2 }}"></p>
+              <p class="review-count">{{ $recently_product->reviews->count() }}</p>
+              @endif
+            </div>
+            @else
+            <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
+            @endif
+          </a>
+          <div class="row">
+            <div class="col-12">
+              <p class="samuraimart-product-label mt-2">
+                {{ $recently_product->name }}<br>
+                <label>¥{{ $recently_product->price }}</label>
+              </p>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
+</div>
+@endsection
