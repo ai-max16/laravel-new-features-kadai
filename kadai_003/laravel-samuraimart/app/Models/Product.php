02c6780163052a0a076c612af1b68dd504d23000
ai-max16 <renn8q8@gmail.com>
2023-12-27 13:28:05 +0900

    kadai_003を提出します

---
+++ kadai_003/laravel-samuraimart/app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelFavorite\Traits\Favoriteable;
+use Kyslik\ColumnSortable\Sortable;
+
+class Product extends Model
+{
+    use HasFactory, Favoriteable, Sortable;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'image',
+        'recommend_flag',
+        'carriage_flag',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+     {
+         return $this->hasMany(Review::class);
+     }
+
+     public function reviewSortable($product, $direction)
+    {
+        return $product->withAvg('reviews', 'score')
+                       ->orderBy('reviews_avg_score', $direction);
+    }
+}
